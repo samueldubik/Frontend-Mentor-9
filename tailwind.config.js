dd1bcb09c057359d58a01973c0f8d7dbf78dfa4c
Samuel Dubik <samuel.dubik@gmail.com>
2022-02-13 13:16:13 +0100

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'red'      : '#ea5353',
+      'cyan'     : '#45d3d3',
+      'orange'   : '#fcaf4a',
+      'blue'     : '#549ef2',
+      'blue-dark': '#4c4e61',
+      'blue-gray': '#a3a5ae',
+      'gray'     : '#fafafa',
+    },
+
+    fontFamily: {
+        'custom' : ['Poppins']
+    }
+  },
+  plugins: [],
+}
